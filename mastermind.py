1710cd5ecacadf6604b092fff6ec568f0fce8075
Sneebog <Sethbaldrey@gmail.com>
2024-01-17 17:53:03 +0000

    valid function made

--- mastermind.py
+++ mastermind.py
@@ -1,4 +1,13 @@
 import random 
+import sys
+
+# #For a human game
+# inputfile  = sys.argv[1]
+# outputfile = sys.argv[2]
+# codelength = sys.argv[3]
+codelength = 4
+#colour dictionary 
+colour_dictionary = ["red", "blue", "yellow", "green", "orange"]
 
 def generatecode(): #create a random code
     code = ""
@@ -18,14 +27,26 @@ def generatecode(): #create a random code
             code += "purple "
     return code
 
-def get_guess(guessstring):
-    guess = guessstring.split() #turn the guesses into an array
-    return guess
+def valid(code):
+    #check there is valid guesses 
+    if len(code) == codelength: #valid length
+        valid = True
+        for colour in code:
+            if not (colour in colour_dictionary ):
+                valid = False
+    else:
+        valid = False
+
+    if valid:
+        return code
+    else:
+        return []
 
 def give_feedback(code, guess):
-    if len(code) == len(guess):
+    guess = valid(guess)
+    if guess != []:
         colour_pins = [""] * len(guess)#make colour pins list
-        colour_set  = []
+        colour_set  = [] #to ensure a white peg cant get assigned if colour is already assigned
         for i in range(len(guess)):#loop through the guesses
             if code[i] == guess[i]: #if the guess is the right colour in right position
                 colour_pins[i] = "black"
@@ -34,8 +55,6 @@ def give_feedback(code, guess):
 
         for i in range(len(guess)):#loop through the guesses
             for j in range(len(code)):
-                #if colour_pins[j] == "":
-                    #white_flag = False #to ensure it doesnt make two white for one colour
                 if code[j] == guess[i] and colour_pins[j] == "" and not (code[j] in colour_set): #if the guess is the right colour wrong position
                         colour_pins[j] = "white"
                         colour_set.append(code[j])
@@ -46,25 +65,26 @@ def give_feedback(code, guess):
     else:
         return " ill-formed guess provided"
 
-def guess(code, mode, guessarray = None,  max_guess = 10): #the main guessing function 
+def guess(code, mode, guessarray = None,  max_guess = 12): #the main guessing function 
     guess_num = 0
     solved = False
-    outputfile = open("outputexample", "w")
+    file = open("outputfile", "w")
     while guess_num != max_guess and not solved: #loop until the maximum guesses are reached and until it is solved
         guess_num += 1 #increment the guess number 
         if mode == "test":
-            guess = input("Enter Guess: ")
+            guess_string = input("Enter Guess: ")
             print("")
         elif mode == "human":
-            guess = guessarray[guess_num - 1]
+            guess_string = guessarray[guess_num - 1]
         #elif mode == "computer"
         #   computer(code, feedback)
-        feedback = give_feedback(code, get_guess(guess)) #get the feedback from the guess
+        guess = guess_string.split()
+        feedback = give_feedback(code, guess) #get the feedback from the guess
         if mode == "test":
             print("Guess ", guess_num, ":", feedback) #print the feedback
         else:
             text = "Guess " + str(guess_num) + ":" + str(feedback) + "\n"
-            outputfile.write(text)
+            file.write(text)
         feedback = feedback.split() 
         if len(feedback) == len(code): #check if the feedback is the same length as the the code (Each peg is correct)
             feedback_flag = True #use flag to check each peg is correct 
@@ -81,19 +101,15 @@ def guess(code, mode, guessarray = None,  max_guess = 10): #the main guessing fu
         print("The game was completed. Further lines were ignored.")
     else:
         if solved: #return end statements
-            outputfile.write("You won in " + str(guess_num) + " guesses. Congratulations!\n")
+            file.write("You won in " + str(guess_num) + " guesses. Congratulations!\n")
         else:
-            outputfile.write("You lost the code was:" + str(code) + "\n")
-        outputfile.write("The game was completed. Further lines were ignored.\n")
-    outputfile.close()
-code = generatecode()
-codearray = code.split()
-#print(code)
-#guess(10, codearray)
+            file.write("You lost the code was:" + str(code) + "\n")
+        file.write("The game was completed. Further lines were ignored.\n")
+    file.close()
 
 
 
-print(give_feedback(["r","b", "o", "g"], ["p", "b", "g", "g"]))
+print(give_feedback(["blue","red", "orange", "yellow"], ["red", "green", "yellow", "yellow"]))
 
 #code for opening files and outputting them
 
@@ -118,6 +134,7 @@ def mastermind(filearray):
         mode = mode[1]
         guessarray = filearray[2:]     
         guess(code, mode, guessarray)
+    else:
+        print("")
 
-
-mastermind(readfile("inputexample1.txt"))
+#mastermind(readfile("inputexample1.txt"))
