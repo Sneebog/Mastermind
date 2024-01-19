063317cfd6682092180fdbdbd111e8ba88de8f4d
Sneebog <Sethbaldrey@gmail.com>
2024-01-19 00:01:59 +0000

    version with test case

--- mastermind.py
+++ mastermind.py
@@ -1,10 +1,11 @@
 import random 
 import sys
+from  pathlib import Path
 
 # #For a human game
 num_args = len(sys.argv)
-print (num_args)
-if num_args >= 4: 
+
+if num_args >= 3: 
     valid_args = True  
     input_file  = sys.argv[1]
     output_file = sys.argv[2]
@@ -21,6 +22,8 @@ codelength = 3
 colour_dictionary = ["red", "blue", "yellow", "green", "orange"]
 # if colours != []:
 #     colour_dictionary += colours
+
+output_file = "outputfile.txt"
 
 def generateguess(): #create a random code
     code = ""
@@ -41,17 +44,10 @@ def valid(code):
         valid = False
     return valid
 
-def valid_file(lines):
-    #check the inputfile is valid
-    if len(lines) > 2:
-        codestring,modestring = lines[0], lines[1]
-        code, mode = codestring.split(), modestring.split()
-        if code[0] == "code" and mode[0] == "player":
-            return True, code, mode
-        else:
-            return False, [], []
-    else:
-        return False, [], []
+def valid_file(file):
+    #check the file is valid
+    path = Path(file)
+    return path.is_file()
 
         
 def give_feedback(code, guess):         
@@ -79,21 +75,22 @@ def give_feedback(code, guess):
 def guess(code, mode, guessarray = None,  max_guess = 12): #the main guessing function 
     guess_num = 0
     solved = False
-    #file = open("outputfile", "w")
     file = open(output_file, "w")
+    if mode == "computer":
+        comp_file = open("computerGame.txt", "w")
     while guess_num != max_guess and not solved: #loop until the maximum guesses are reached and until it is solved
         guess_num += 1 #increment the guess number 
         if mode == "test":
             guess_string = input("Enter Guess: ")
             print("")
         elif mode == "human":
-            if guess_num < len(guessarray):
+            if guess_num < len(guessarray) + 1:
                 guess_string = guessarray[guess_num - 1]
             else:
                 break
         elif mode == "computer":
-          #computer(code, feedback)
             guess_string = generateguess()
+            comp_file.write(guess_string)
         guess = guess_string.split()
         feedback = give_feedback(code, guess) #get the feedback from the guess
         if len(feedback) == codelength:
@@ -121,9 +118,14 @@ def guess(code, mode, guessarray = None,  max_guess = 12): #the main guessing fu
     else:
         if solved: #return end statements
             file.write("You won in " + str(guess_num) + " guesses. Congratulations!\n")
+            file.write("The game was completed. Further lines were ignored.\n")
         else:
-            file.write("You lost the code was: " + " ".join(code) + "\n")
-        file.write("The game was completed. Further lines were ignored.\n")
+            #file.write("You lost the code was: " + " ".join(code) + "\n")
+            file.write("You lost. Please try again.\n")
+            if guess_num == max_guess:
+                file.write("You can only have " + str(max_guess) + " guesses.\n")
+    if mode =="computer":
+        comp_file.close()
     file.close()
 
 #code for reading the file
@@ -134,19 +136,26 @@ def readfile(file):
    
 def mastermind(valid_args):
     if valid_args:
-        lines = readfile(input_file)
-        is_valid, code, mode = valid_file(lines)
-        if is_valid:
-            code = code[1:]
-            if valid(code):
-                if len(mode) == 2 and (mode[1] == "human" or mode[1] == "computer"):
-                    guess_lines = lines[2:]
-                    guess(code, mode[1], guess_lines)
-                    return 0 #The programme ran successfully
+        if valid_file(input_file):
+            if valid_file(output_file):
+                lines = readfile(input_file)
+                if len(lines) > 2:
+                    codestring,modestring = lines[0], lines[1]
+                    code, mode = codestring.split(), modestring.split()
+                    if code[0] == "code" and valid(code[1:]):
+                        code = code[1:]
+                        if len(mode) == 2 and mode[0] == "player" and (mode[1] == "human" or mode[1] == "computer"):
+                            guess_lines = lines[2:]
+                            guess(code, mode[1], guess_lines)
+                            return 0 #The programme ran successfully
+                        else:
+                            return 5 #No or ill-formed player provided
+                    else:
+                        return 4 #No or ill-formed code provided 
                 else:
-                    return 5 #No or ill-formed player provided")
+                    return 3 #There was an issue with the output file 
             else:
-                return 4 #No or ill-formed code provided"
+                return 3 #There was an issue with the output file 
         else:
             return 2 #There was an issue with the input file       
     else:
